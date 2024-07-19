c8c21d6e26eeb8c58603fd37960cc9a54663ad80
FabioPortella <fabioportella2009@gmail.com>
2024-07-19 12:08:50 -0300

    retirada de hash em senha do usuário que será implementada no site Next.JS

--- users/api.py
+++ users/api.py
@@ -1,7 +1,5 @@
 from ninja import Router
-from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
-from rolepermissions.roles import assign_role
 
 from .schemas import MessageSchema, UserSchema 
 from .models import User
@@ -11,9 +9,9 @@ users_router = Router()
 
 @users_router.post('/', response={201: UserSchema, 400: MessageSchema, 500: MessageSchema})
 def create_user(request, user: UserSchema):
+    print(user.dict())
     try:
         user = User(**user.dict())
-        user.password = make_password(user.password)
         user.full_clean()  # usar para reconhecer os validators
         user.save()
         return 201, user
